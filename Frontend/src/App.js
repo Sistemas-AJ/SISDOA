8fc3cf413e134692239b003792fa63a881bd7aea
Adrian Ruiz <adrianalejandroruiz13@gmail.com>
2025-09-30 14:44:10 -0500

    Barra superior hecha

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import BarraInferior from './components/BarraInferior/BarraInferior';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-         <code>hola mundo</code>
-      </header>
-    </div>
+    <Router>
+      <BarraInferior />
+      {/* Aquí puedes agregar tus rutas principales */}
+      <div className="App">
+        <header className="App-header">
+          <code>hola mundo</code>
+        </header>
+      </div>
+    </Router>
   );
 }
 
