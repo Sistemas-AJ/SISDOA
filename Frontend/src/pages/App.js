6e5fabb6d5665f73a0cc4f645b3987c32c3eaf9b
Adrian Ruiz <adrianalejandroruiz13@gmail.com>
2025-09-30 12:50:40 -0500

    backend inicado

---
+++ Frontend/src/pages/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+         <code>hola mundo</code>
+      </header>
+    </div>
+  );
+}
+
+export default App;
